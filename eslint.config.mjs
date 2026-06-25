35663a07bef97eeeced482cfe3d21342c6f7ca6e
xps-admin <strategicmindsadvisory@gmail.com>
2026-06-25 17:55:21 -0400

    add: eslint.config.mjs

---
+++ eslint.config.mjs
@@ -0,0 +1,4 @@
+import { FlatCompat } from "@eslint/eslintrc";
+const compat = new FlatCompat({ baseDirectory: import.meta.dirname });
+const config = [...compat.extends("next/core-web-vitals")];
+export default config;
